9e70762f7f5d22e133dc1115d6e7a1de8f467787
KevinDonkers <kevindonkers1@hotmail.com>
2016-04-08 10:34:05 -0400

    added a textclock and made the 24 hour switch to switch the textclocks time format

--- app/src/main/java/ca/georgiancollege/time_ticker_app/MainActivity.java
+++ app/src/main/java/ca/georgiancollege/time_ticker_app/MainActivity.java
@@ -9,8 +9,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextClock;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Switch clockFormatSwitch;
+    private TextClock mainClock;
+    private CharSequence default24HourFormat, default12HourFormat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +27,32 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        clockFormatSwitch = (Switch) findViewById(R.id.clockFormatSwitch);
+        mainClock = (TextClock) findViewById(R.id.textClock);
+
+        default12HourFormat = mainClock.getFormat12Hour();
+        default24HourFormat = mainClock.getFormat24Hour();
+
+        //attach a listener to check for changes in state
+        clockFormatSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView,
+                                         boolean isChecked) {
+                //if the switch is activated set the clock to 24 hour mode
+                if(isChecked){
+                    mainClock.setFormat24Hour(default24HourFormat);
+                    mainClock.setFormat12Hour(null);
+                }else{
+                    mainClock.setFormat12Hour(default12HourFormat);
+                    mainClock.setFormat24Hour(null);
+                }
+
+            }
+        });
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -27,6 +60,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
 
     public void openAlarmScreen(){
         Intent openAlarmIntent = new Intent(MainActivity.this, AddAlarmActivity.class);
